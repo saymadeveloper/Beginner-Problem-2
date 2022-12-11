c5f7d9dc9f27b6d0a6d333701bb04fc77476a6ba
saymadeveloper <mousaymaafroj2@gmail.com>
2022-12-12 02:17:15 +0600

    First commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav class="navbar navbar-light bg-white border-bottom fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <img src="/images/logo.png" />
+        </a>
+        <ul className="navbar-nav me-2 menu">
+          <li className="nav-item">
+            <a className='logIn' aria-current="page" href="#">Log in</a>
+          </li>
+          <li className="nav-item">
+            <a className='join' aria-current="page" href="#">Join for free</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+  );
+}
